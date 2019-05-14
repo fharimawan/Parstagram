97ff36d43fe57292f7759a7f0b8a2e03101e22f7
Farel Harimawan <farel@dhcp-10-8-046-103.mobile.reshsg.uci.edu>
2019-05-13 23:54:21 -0700

    completed required user stories but tableview won't reload

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.server = "https://floating-citadel-18223.herokuapp.com/parse"
+            }))
+        
         return true
     }
 
